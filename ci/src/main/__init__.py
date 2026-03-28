fe13ea4f2415a2e97735ac16a2d61d7cab0540ef
Frederic Leger <frederic@webofmars.com>
2026-03-28 17:47:05 +0100

    ci: upgrade dagger

--- ci/src/main/__init__.py
+++ ci/src/main/__init__.py
@@ -1,4 +1,3 @@
-import json
 import dagger
 from dagger import dag, function, object_type
 
@@ -6,40 +5,45 @@ from dagger import dag, function, object_type
 class DockerColors:
 
     @function
-    async def build(self, source: dagger.Directory , color: str = "blue") -> list[dagger.Container]:
+    async def build(self, source: dagger.Directory, color: str = "blue") -> list[dagger.Container]:
         color_arg = "COLOR=%s" % color
-        platform = [dagger.Platform("linux/amd64"), dagger.Platform("linux/arm64")]
-        dag = dagger.Client()
         print("Building image for color %s" % color)
-        print("Directory: %s" % source.id)
         img = (
             dag.docker()
             .build(
                 dir=source,
-                platform=platform,
+                platform=[
+                  dagger.Platform("linux/amd64"),
+                  dagger.Platform("linux/arm64")
+                ],
                 args=[color_arg]
             )
         )
-        print("Img: %s" % repr(img.image()))
-        return [img.image(platform=platform)]
-
-    @function
-    async def push(self, image: dagger.Container) -> None:
-        print("Pushing image %s" % image.id)
-        dag = dagger.Client()
-        dag.docker().push(image)
-
-    @function
-    # login to the docker registry
-    async def login(self) -> None:
-        dag = dagger.Client()
-        dag.docker().login()
-
-    @function
-    def ci(self, source: dagger.Directory) -> None:
-        colors = ["red", "green", "blue", "yellow", "purple"]
-        platforms = [
-            dagger.Platform("linux/amd64"), dagger.Platform("linux/arm64")
+        return [
+          img.image(platform=dagger.Platform("linux/amd64")),
+          img.image(platform=dagger.Platform("linux/arm64"))
         ]
+
+    @function
+    async def ci(self, source: dagger.Directory) -> None:
+        colors = [
+          "blue",
+          "brown",
+          "gold",
+          "gray",
+          "green",
+          "lime",
+          "orange",
+          "pink",
+          "purple",
+          "red",
+          "silver",
+          "teal",
+          "white",
+          "yellow"
+        ]
+
         for color in colors:
-            self.build(self, source=source, color=color, platforms=platforms)
+            print("building docker image for variant=%s" % color)
+            variants = await self.build(source=source, color=color)
+            await dag.container().publish("webofmars/colors:%s" % color, platform_variants=variants)
